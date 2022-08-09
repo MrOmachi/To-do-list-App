b00a5ae5a43b42692fb16598676408bc34170759
Omachi John <omachi.john@.com>
2022-08-09 20:12:08 +0100

    Set index,js file to src folder and main.scss file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import './styles/main.scss';
-// import CreatToDo from './modules/domDisplay.js';
+import './styles/main.scss';
+import CreatToDo from './modules/domDisplay.js';
 
-// CreatToDo();
+CreatToDo();
