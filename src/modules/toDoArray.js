0c4856a48f6fbc114df741f7e413fdd74ae44f14
Omachi John <omachi.john@.com>
2022-08-09 20:12:48 +0100

    Add module for todo list array

--- src/modules/toDoArray.js
+++ src/modules/toDoArray.js
@@ -1,24 +1,24 @@
-// const toDoArr = [
-//   {
-//     id: 1,
-//     description: 'Complete daily Task',
-//     completed: true,
-//   },
-//   {
-//     id: 2,
-//     description: 'Complete daily project',
-//     completed: true,
-//   },
-//   {
-//     id: 3,
-//     description: 'Complete weekly restrospectives',
-//     completed: true,
-//   },
-//   {
-//     id: 4,
-//     description: 'Complete monthly capestone',
-//     completed: true,
-//   },
-// ];
+const toDoArr = [
+  {
+    id: 1,
+    description: 'Complete daily Task',
+    completed: true,
+  },
+  {
+    id: 2,
+    description: 'Complete daily project',
+    completed: true,
+  },
+  {
+    id: 3,
+    description: 'Complete weekly restrospectives',
+    completed: true,
+  },
+  {
+    id: 4,
+    description: 'Complete monthly capestone',
+    completed: true,
+  },
+];
 
-// export default toDoArr;
+export default toDoArr;
