e407f95675a82e5a5243965cf65b37c48bdcef82
Omachi John <omachi.john@.com>
2022-08-19 13:15:58 +0100

    fix eslint errors

--- src/modules/__test__/localStorage.test.js
+++ src/modules/__test__/localStorage.test.js
@@ -1,3 +1,78 @@
+import Storage from '../localStorage.js';
+
+// eslint-disable-next-line func-names
+const localStorageMock = (function () {
+  let store = {};
+
+  return {
+    getItem(key) {
+      return store[key];
+    },
+
+    setItem(key, value) {
+      store[key] = value;
+    },
+
+    clear() {
+      store = {};
+    },
+
+    removeItem(key) {
+      delete store[key];
+    },
+
+    getAll() {
+      return store;
+    },
+  };
+}());
+
+global.localStorage = localStorageMock;
+
+describe('Task one: add and delete', () => {
+  test('data is added into local storage', () => {
+    const mockTodo = {
+      description: 'json data',
+      id: 1,
+      completed: false,
+    };
+
+    const mockTodo2 = {
+      description: 'jso',
+      id: 2,
+      completed: false,
+    };
+
+    const expected = [
+      {
+        id: 1,
+        description: 'json data',
+        completed: false,
+      },
+      {
+        description: 'jso',
+        id: 2,
+        completed: false,
+      },
+    ];
+    Storage.addTodo(mockTodo);
+    Storage.addTodo(mockTodo2);
+    expect(Storage.getToDo()).toEqual(expected);
+  });
+
+  test('Data is removed from local storage', () => {
+    const expected = [
+      {
+        description: 'json data',
+        id: 1,
+        completed: false,
+      },
+    ];
+    Storage.remove(2);
+    expect(Storage.getToDo()).toStrictEqual(expected);
+  });
+});
+
 describe('task2', () => {
   // Remove all completed
   test('Remove all completed with the status of true', () => {
